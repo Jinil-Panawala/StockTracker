3e5faf725cd34e416e97590fd996aeaa4622f1fc
Jinil Panawala <jinilpanawala2004@gmail.com>
2024-04-21 01:01:57 -0400

    __dirname

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 
-const _dirname = path.dirname("");
+const _dirname = __dirname;
 const buildPath = path.join(_dirname , "../client/build");
 
 app.use(express.static(buildPath));
