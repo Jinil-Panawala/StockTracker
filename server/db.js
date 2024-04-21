6ac1ec265a2e9760c07c137838bbf9d2682cba23
Jinil Panawala <jinilpanawala2004@gmail.com>
2024-04-20 23:46:48 -0400

    Project restructuring

--- server/db.js
+++ server/db.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-const cluster = 'stockwatchlistcluster';
-const db = 'savedStocks';
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(`mongodb+srv://jinilpanawala:Jinil2004@${cluster}.ekd6io8.mongodb.net/${db}?retryWrites=true&w=majority&appName=StockWatchlistCluster`);
+        const connection = await mongoose.connect(process.env.MONGODBURI);
         console.log(`MongoDB Connected: ${connection.connection.host}`);
     } catch (error) {
         console.error(error);
